e40eebbb1cabbf1d14db4c6320fbb75e1296ef93
yzviruk <zvirukdev@gmail.com>
2026-05-06 15:20:37 +0300

    river correction

--- web/app.js
+++ web/app.js
@@ -88,8 +88,8 @@ slider.noUiSlider.on("change", (vals) => {
 showZone(5);
 
 // Rivers layer
-const RIVER_STYLE = { color: "#1f77c4", weight: 1.4, opacity: 0.85 };
-const STREAM_STYLE = { color: "#1f77c4", weight: 1.0, opacity: 0.7 };
+const RIVER_STYLE = { color: "#1f77c4", weight: 1.4, opacity: 1.0 };
+const STREAM_STYLE = { color: "#1f77c4", weight: 1.0, opacity: 0.9 };
 
 let riversLayer = null;
 let streamsLayer = null;
